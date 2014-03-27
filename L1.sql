7fb7eb570b900b9c8e30796aa7eb76107c6719c7
dzianis_lahutsik <dzianis_lahutsik@epam.com>
2014-03-28 00:02:49 +0300

    done

--- L1.sql
+++ L1.sql
@@ -30,7 +30,7 @@ comment on column TEST_TABLE.name is 'Another comment';
 
 create index TEST_TABLE_NAME_INDEX on TEST_TABLE(name);
 
-alter table TEST_TABLE add constraint name_check CHECK(name like 'A%');
+alter table TEST_TABLE add constraint name_check check(name like 'A%');
 /* test */
 insert into TEST_TABLE(id, name) values(1, 'qwert)';
 insert into TEST_TABLE(id, name) values(1, 'Aqwert)';
@@ -69,3 +69,29 @@ create table TABLE_WITH_CLUSTER (
 	id number(10),
 	title varchar(100)
 ) cluster TEST_CLUSTER(id);
+
+/* 4 */
+create table T_TBL (
+	id number(10);
+	title varchar(100)
+);
+alter table T_TBL add constraint title_check check(title like 'A%');
+
+insert into T_TBL(id, title) values(1, 'Abc');
+	insert into T_TBL(id, title) values(2, 'Abcd');
+		insert into T_TBL(id, title) values(3, 'Abcde');
+			insert into T_TBL(id, title) values(4, 'Abcdef');
+				insert into T_TBL(id, title) values(5, 'Abcdefg')
+					insert into T_TBL(id, title) values(null, 'Abcdefgh')
+
+alter table T_TBL add new_column number(5);
+alter table T_TBL add new_column_with_default number(5) default 42;
+alter table T_TBL modify title char(100) default 'AAA';
+alter table T_TBL rename column new_column to count;
+alter table T_TBL drop column new_column_with_default;
+alter table T_TBL modify title default null;
+alter table T_TBL disable constraint title_check;
+
+rename T_TBL to T_TBL_renamed;
+
+create table T_TBL as (select * from T_TBL_renamed);
