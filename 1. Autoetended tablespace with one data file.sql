2e38572396969457a1a47253544fe9d1fe26770d
aleh_atsman <aleh_atsman@epam.com>
2014-03-31 08:36:48 +0400

    DML 1. dictionaries and one master table

--- 1. Autoetended tablespace with one data file.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLESPACE YOUR_HOME
-  DATAFILE 'yourhome.dat' SIZE 50M AUTOEXTEND ON;
-
-SELECT tablespace_name, file_name, status, bytes
-  FROM dba_data_files
-  WHERE tablespace_name LIKE 'YOUR_HOME'
-  ORDER BY tablespace_name, file_name;
