a69d37053057b72ca1363000fca136728750371b
Alexey Vakulich <soulSpringg@gmail.com>
2014-04-24 22:53:20 +0300

    1.0.1

--- Transaction_Isolation_Level.sql
+++ Transaction_Isolation_Level.sql
@@ -1,20 +1,39 @@
-/*----------------Dirty-Reads for Read Uncommitted-------------------*/
 create table employee (
-	f1 number(10),
-	f2 number(10)
+	example_number_1 number(10),
+	example_number_2 number(10)
 );
 
-insert into employee(f1, f2) VALUES(1, 2);
+insert into employee(example_number_1, example_number_2) VALUES(1, 2);
+
+/*----------------Dirty-Reads in Read Uncommitted level-------------------*/
 
 /*Transaction A*/
-update employee SET f2=f2+1 WHERE f1=1;
+update employee SET example_number_2=example_number_2+1 WHERE example_number_1=1;
 
 /*Transaction B*/
-select f2 from employee WHERE f1=1;
+select example_number_2 from employee WHERE example_number_1=1;
 
 rollback work;
-/*In transaction A modifies the value of the transaction field f2,
+/*In transaction A modifies the value of the transaction field example_number_2,
 and then in transaction B value of this fields is selected.
 Thereafter, transaction A is rolled back. Resulting in a value
 obtained by the transaction B that is different from the value
 stored in the database.*/
+
+/*----------------Non-Repeatable Reads in Read Committer level-------------------*/
+
+/*Transaction A*/
+select example_number_2 from employee WHERE example_number_1=1;
+
+/*Transaction B*/
+select example_number_2 from employee WHERE example_number_1=1;
+
+/*Transaction A*/
+update employee SET example_number_2=example_number_2+1 WHERE example_number_1=1;
+commit;
+
+/*Transaction B*/
+select example_number_2 from employee WHERE example_number_1=1;
+/*When the transaction tries to get the value
+of it will be different from the initial value in this
+transaction*/
